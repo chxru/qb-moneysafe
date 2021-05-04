0ea99bb0243b52c936df932893fff2d73a06555d
Charuka Samarakoon <charuka@protonmail.com>
2021-05-04 22:26:47 +0000

    Potential fix for 'transcation' can't have default value

--- qb-moneysafe.sql
+++ qb-moneysafe.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `moneysafes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `safe` varchar(50) NOT NULL DEFAULT '0',
   `money` int(11) NOT NULL DEFAULT 0,
-  `transactions` text NOT NULL DEFAULT '0',
+  `transactions` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
